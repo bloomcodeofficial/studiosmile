7db5ed585eb3dc0adc6c965e5cd818804bbe27ef
niklashans <104555311+niklashans@users.noreply.github.com>
2022-11-20 20:55:04 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-document.querySelector('[bw-element='portfolio-item']')?.onclick(function() {
+document.querySelector('[bw-element='play-button']')?.onclick(function() {
 
   document.querySelector('[bw-element='media-player']')?.style.display = 'flex';
 
