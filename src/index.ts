d9f95afc8ab16f1224ac62c2ddfe8dcfe78643ee
niklashans <104555311+niklashans@users.noreply.github.com>
2022-11-20 21:38:57 +0100

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
+import { greetUser } from '$utils/greet';
+
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  
-  const portfolioItem = document.querySelectorAll('[bw-element='portfolio-item']')!;
-
-  const mediaPlayer = document.querySelectorAll('[bw-element='media-player']')!;
-
-  const playButton = document.querySelectorAll('[bw-element='play-button']')!;
-
-  playButton?.addEventListener('click', function handleClick() {
-    mediaPlayer.style.display = 'block';
-  })
-
+  const name = 'John Doe';
+  greetUser(name);
 });
-
-
-
-
-
-
-  
