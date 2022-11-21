11816f0ad48e45158247623414d3561798b68b6d
niklashans <104555311+niklashans@users.noreply.github.com>
2022-11-21 08:13:18 +0100

    Update studiosmile.js

--- src/studiosmile.js
+++ src/studiosmile.js
@@ -2,6 +2,6 @@ const portfolioItem = document.querySelector('[bw-element="portfolio-item"]');
 
 const mediaPlayer = document.querySelector('[bw-element="media-player"]');
 
-const myName = 'Nikllas';
-
-console.log(myName);
+document.querySelector('[bw-element="portfolio-item"]').mouseenter(function() {
+     console.log('Hello legend');
+})
